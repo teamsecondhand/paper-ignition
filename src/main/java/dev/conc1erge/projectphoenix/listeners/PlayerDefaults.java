5d3c9ee8d16ab8ec36471a4a685ecb9f1fab56a6
conc1erge <29669519+conc1erge@users.noreply.github.com>
2024-09-18 23:19:04 +0800

    version 1.4-predev release

--- src/main/java/dev/conc1erge/projectphoenix/listeners/PlayerDefaults.java
+++ src/main/java/dev/conc1erge/projectphoenix/listeners/PlayerDefaults.java
@@ -1,5 +1,6 @@
 package dev.conc1erge.projectphoenix.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Sound;
 import org.bukkit.SoundCategory;
@@ -11,14 +12,23 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerItemDamageEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.event.server.ServerLoadEvent;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 import java.util.Date;
 
 public class PlayerDefaults implements Listener {
+    Scoreboard score = Bukkit.getScoreboardManager().getMainScoreboard();
+    @EventHandler // Team for hiding nametags
+    private void hideNametags(ServerLoadEvent event) { // sets a team for hiding nametags, this may or may not work but im leaving it here anyway -c1
+        Team team = score.getTeam("namehide");
+        if(team == null) {
+            team = score.registerNewTeam("namehide");
+            team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER);
+        }
+    }
 
     @EventHandler // Player Defaults on Join
     public void onPlayerJoin(PlayerJoinEvent e) {
@@ -29,6 +39,19 @@ public class PlayerDefaults implements Listener {
         p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40.0); // Ensures that the player on join DOES get 40 HP/20 hearts
         p.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(16.01);
         p.getAttribute(Attribute.PLAYER_SWEEPING_DAMAGE_RATIO).setBaseValue(0.2);
+
+        if (p.hasPlayedBefore()) {
+            p.setGameMode(GameMode.SPECTATOR);
+        }
+
+        // Set player's nametag hidden on join
+        score.getTeam("namehide").addEntry(p.getName());
+    }
+
+    @EventHandler // Player Defaults on Leave
+    public void onPlayerLeave(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+        p.setHealth(0.00); // pseudo-combat log? hopefully that works
     }
 
     @EventHandler
@@ -37,6 +60,21 @@ public class PlayerDefaults implements Listener {
         p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40.0);
         p.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(16);
         p.getAttribute(Attribute.PLAYER_SWEEPING_DAMAGE_RATIO).setBaseValue(0.1);
+        p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.1);
+        p.getAttribute(Attribute.GENERIC_SCALE).setBaseValue(1);
+
+        // Only here if the player is not in the team, coming from respawn
+        score.getTeam("namehide").addEntry(p.getName());
+    }
+
+    @EventHandler // Removes player from the nametag hide team in spec, future proof for staff, might be removed later when new nametag system is implemented -c1
+    public void onPlayerSwitchGamemode(PlayerGameModeChangeEvent e) {
+        Player p = e.getPlayer();
+        if (p.getGameMode() == GameMode.SPECTATOR) {
+            score.getTeam("namehide").removeEntry(p.getName());
+        } else {
+            score.getTeam("namehide").addEntry(p.getName());
+        }
     }
 
     @EventHandler
