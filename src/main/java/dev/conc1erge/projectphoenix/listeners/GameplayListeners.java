aec5621194c2c3b2d5af132607d738926250a8bc
conc1erge <29669519+conc1erge@users.noreply.github.com>
2024-09-24 01:06:18 +0800

    1.5 predev milestone: combat log
    combat log on the java side has been added, now we're in a state where i think most of the heavy gameplay lifting is in skript now and active development should be pushed there, unless bob stinks

--- src/main/java/dev/conc1erge/projectphoenix/listeners/GameplayListeners.java
+++ src/main/java/dev/conc1erge/projectphoenix/listeners/GameplayListeners.java
@@ -1,18 +1,73 @@
 package dev.conc1erge.projectphoenix.listeners;
 
 import dev.conc1erge.projectphoenix.IgnitionCore;
-import org.bukkit.event.EventHandler;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class GameplayListeners implements Listener {
-    @EventHandler
-        public void combatCheck (){
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    // TODO: Make a combat logging system here, nothing too harsh, simply has to be basic and kills player on disconnect.
-                }
-        }.runTaskTimer(IgnitionCore.plugin, 0, 20);
+
+    static int combatLogTime = 15;
+    // set this value to how long the combat log is in seconds
+
+    // Combat Logging Checker
+    public static class combatCheck {
+        private static HashMap<UUID, combatTimer> Timer = new HashMap<UUID, combatTimer>();
+
+        public static void add(UUID uuid) {
+            if (hasTimer(uuid))
+                remove(uuid);
+            Timer.put(uuid, new combatTimer(uuid));
+        }
+
+        public static boolean hasTimer(UUID uuid) {
+            return Timer.containsKey(uuid);
+        }
+
+        public static boolean remove(UUID uuid) {
+            if (hasTimer(uuid)) {
+                Timer.get(uuid).cancel();
+                Timer.remove(uuid);
+            }
+            return false;
+        }
+
+    }
+
+    // Combat Logging Timer
+    public static class combatTimer extends BukkitRunnable {
+        private int time;
+        private UUID uuid;
+        private Player player;
+
+        public combatTimer(UUID uuid) {
+            this.time = combatLogTime; // change this for combat time check thing
+            this.uuid = uuid;
+            player = Bukkit.getPlayer(uuid);
+            this.runTaskTimer(IgnitionCore.plugin, 0, 20L);
+        }
+
+        @Override
+        public void run() {
+            if (!player.isOnline()) {
+                if (combatCheck.hasTimer(uuid))
+                    combatCheck.remove(uuid);
+                cancel();
+                return;
+            }
+
+            if (time <= 0) {
+                if (combatCheck.hasTimer(uuid))
+                    combatCheck.remove(uuid);
+                cancel();
+                return;
+            }
+            time--;
+        }
     }
 }
--- src/main/java/dev/conc1erge/projectphoenix/listeners/PlayerDefaults.java
+++ src/main/java/dev/conc1erge/projectphoenix/listeners/PlayerDefaults.java
@@ -1,26 +1,26 @@
 package dev.conc1erge.projectphoenix.listeners;
 
-import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
-import org.bukkit.Sound;
-import org.bukkit.SoundCategory;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.EntityRegainHealthEvent;
-import org.bukkit.event.entity.FoodLevelChangeEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.*;
 import org.bukkit.event.player.*;
 import org.bukkit.event.server.ServerLoadEvent;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
+import dev.conc1erge.projectphoenix.listeners.GameplayListeners.combatCheck;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Date;
+import java.util.UUID;
 
 public class PlayerDefaults implements Listener {
+
     Scoreboard score = Bukkit.getScoreboardManager().getMainScoreboard();
+
     @EventHandler // Team for hiding nametags
     private void hideNametags(ServerLoadEvent event) { // sets a team for hiding nametags, this may or may not work but im leaving it here anyway -c1
         Team team = score.getTeam("namehide");
@@ -39,19 +39,17 @@ public class PlayerDefaults implements Listener {
         p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40.0); // Ensures that the player on join DOES get 40 HP/20 hearts
         p.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(16);
         p.getAttribute(Attribute.PLAYER_SWEEPING_DAMAGE_RATIO).setBaseValue(0.1);
-
-        if (p.hasPlayedBefore()) {
-            p.setGameMode(GameMode.SPECTATOR);
-        }
-
         // Set player's nametag hidden on join
         score.getTeam("namehide").addEntry(p.getName());
     }
 
     @EventHandler // Player Defaults on Leave
     public void onPlayerLeave(PlayerQuitEvent e) {
-        Player p = e.getPlayer();
-
+        UUID uuid = e.getPlayer().getUniqueId();
+        if (!combatCheck.hasTimer(uuid)) {return;}
+        e.getPlayer().setHealth(0.00);
+        e.getPlayer().setGameMode(GameMode.SPECTATOR);
+        combatCheck.remove(uuid);
     }
 
     @EventHandler
@@ -60,6 +58,7 @@ public class PlayerDefaults implements Listener {
         p.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40.0);
         p.getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(16);
         p.getAttribute(Attribute.PLAYER_SWEEPING_DAMAGE_RATIO).setBaseValue(0.1);
+        // Reset effects
         p.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.1);
         p.getAttribute(Attribute.GENERIC_SCALE).setBaseValue(1);
 
@@ -78,8 +77,9 @@ public class PlayerDefaults implements Listener {
     }
 
     @EventHandler
-    public void itemDurabilityChange(PlayerItemDamageEvent e) {
-        e.setCancelled(true);
+    public void onPlayerHit(EntityDamageByEntityEvent e) {
+        if (!(e.getEntity() instanceof Player)) {return;}
+        combatCheck.add(e.getEntity().getUniqueId());
     }
 
     @EventHandler
@@ -96,12 +96,15 @@ public class PlayerDefaults implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent e) {
         e.getPlayer().getWorld().playSound(e.getPlayer().getLocation(), Sound.ENTITY_GENERIC_EXPLODE, SoundCategory.PLAYERS, 0.5f, 1.0f);
+        UUID uuid = e.getEntity().getUniqueId();
+        if (combatCheck.hasTimer(uuid)) {combatCheck.remove(uuid);}
     }
 
+    @EventHandler // Remove Item Durability Changes
+    public void itemDurabilityChange(PlayerItemDamageEvent e) {e.setCancelled(true);}
+
     @EventHandler // Remove Hunger/Saturation Exhaustion
-    public void onHungerDeplete(FoodLevelChangeEvent e) {
-        e.setCancelled(true);
-    }
+    public void onHungerDeplete(FoodLevelChangeEvent e) {e.setCancelled(true);}
 
     @EventHandler // Prevent Dropping Items TODO: except for select items
     public void onPlayerDropItem(PlayerDropItemEvent e) {
